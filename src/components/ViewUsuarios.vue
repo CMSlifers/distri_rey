260c25ad1e18ef90c88b190c56d011820de68e88
SaTovar <sofiassst28@gmail.com>
2023-09-18 17:46:07 -0500

    En este commit se atualizo el componente usuarios y se rectifico su funcionamiento en la base de datos_ Falta añadir la funcion Limpiar la Tabla

--- src/components/ViewUsuarios.vue
+++ src/components/ViewUsuarios.vue
@@ -1,125 +1,273 @@
 <template>
-    <v-data-table
-      v-model:items-per-page="itemsPerPage"
-      :headers="headers"
-      :items="desserts"
-      item-value="name"
-      class="elevation-1"
-    ></v-data-table>
-  </template>
+  <v-container>
+    <h1>Insertar Datos a Firebase</h1>
+    <v-data-table :headers="headers" :items="desserts" :sort-by="[{ key: 'id', order: 'asc' }]" class="elevation-1">
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>My CRUD</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ props }">
+              <v-btn color="primary" dark class="mb-2" v-bind="props">
+                New Item
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
 
-  <script>
-  export default {
-    data () {
-      return {
-        itemsPerPage: 5,
-        headers: [
-          {
-            title: 'Tabla de Usuarios',
-            align: 'start',
-            sortable: false,
-            key: 'name',
-          },
-          { title: 'Identificacion', align: 'end', key: 'calories' },
-          { title: 'Usuario', align: 'end', key: 'fat' },
-          { title: 'Nombre', align: 'end', key: 'carbs' },
-          { title: 'Direccion', align: 'end', key: 'protein' },
-          { title: 'Email', align: 'end', key: 'iron' },
-          { title: 'Rol', align: 'end', key: 'rol' },
-          { title: 'Contraseña', align: 'end', key: 'pass' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22',
-          },
-          
-        ],
-      }
+              <v-card-text>
+                <v-container>
+                  <v-row>
+
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.id" label="id"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.usuario" label="usuario"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.nombre" label="nombre"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field v-model="editedItem.password" label="password"></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue-darken-1" variant="text" @click="save">
+                  Añadir Nuevo Usuario
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon size="small" class="me-2" @click="editItem(item.raw)">
+          mdi-pencil
+        </v-icon>
+        <v-icon size="small" @click="deleteItem(item.raw)">
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize">
+          Reset
+        </v-btn>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+  
+
+<script>
+
+import db from '../firebase/init.js'
+import { collection, getDocs, query, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore'
+
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        /* title: 'Dessert (100g serving)' */
+        align: 'start',
+        sortable: false,
+        key: 'name',
+      },
+      { title: 'Id', key: 'id' },
+      { title: 'Usuario', key: 'usuario' },
+      { title: 'Nombre', key: 'nombre' },
+      { title: 'password', key: 'password' },
+      { title: 'Actions', key: 'actions', sortable: false },
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      keyid: '',
+      id: 0,
+      usuario: 0,
+      nombre: 0,
+      password: 0,
     },
-  }
+    defaultItem: {
+      name: '',
+      id: 0,
+      usuario: 0,
+      nombre: 0,
+      password: 0,
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    },
+  },
+
+
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+    dialogDelete(val) {
+      val || this.closeDelete()
+    },
+  },
+
+  created() {
+    this.listarUsuarios()
+  },
+
+  methods: {
+
+
+    async eliminarDocumentos() {
+      await deleteDoc(doc(db, "usuarios", this.editedItem.keyid));
+
+    },
+
+
+
+
+    async actualizarUsuarios() {
+      console.log(this.editedItem.keyid)
+      const Ref = doc(db, "usuarios", this.editedItem.keyid);
+      await updateDoc(Ref, {
+        usuario: this.editedItem.usuario,
+        nombre: this.editedItem.nombre,
+        password: this.editedItem.password,
+
+      })
+
+    },
+
+    async createUsuario() {
+      const colRef = collection(db, 'usuarios')
+      console.log(this.editedItem.name, this.editedItem.id, this.editedItem.usuario, this.editedItem.nombre, this.editedItem.password,)
+      const dataObj = {
+        id: this.editedItem.id,
+        usuario: this.editedItem.usuario,
+        nombre: this.editedItem.nombre,
+        password: this.editedItem.password,
+
+      }
+      const docRef = await addDoc(colRef, dataObj);
+      console.log("Creo el usuario con nombre", docRef.id)
+    },
+
+    async listarUsuarios() {
+
+      const q = query(collection(db, "usuarios"));
+      const resul = await getDocs(q);
+      resul.forEach((doc) => {
+
+        console.log("id", doc.id);
+        this.desserts.push({
+          keyid: doc.id,
+          id: doc.data().id,
+          usuario: doc.data().usuario,
+          nombre: doc.data().nombre,
+          password: doc.data().password
+        })
+
+      })
+
+    },
+
+
+    initialize() {
+      this.desserts = [
+        /*         {
+        
+                  id: 2,
+                  usuario: "SaToPi",
+                  nombre: 24,
+                  password: "olis",
+                },
+                {
+        
+                  id: 3,
+                  usuario: "YK2",
+                  nombre: "Carlos Jimenez",
+                  password: "12345",
+                }, */
+      ]
+    },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true;
+
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true;
+
+    },
+
+    deleteItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1)
+      this.closeDelete();
+      this.eliminarDocumentos()
+    },
+
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete() {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+        this.actualizarUsuarios()
+      } else {
+        this.desserts.push(this.editedItem)
+        this.createUsuario()
+      }
+      this.close()
+
+    },
+
+
+  },
+
+}
 </script>
+  
