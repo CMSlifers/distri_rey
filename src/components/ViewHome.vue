ce6ca070116832de2859396133a7528fd5cd890b
CMSlifers <cjimenez1408@gmail.com>
2023-10-02 17:39:18 -0500

    Y2K 0210231738

--- src/components/ViewHome.vue
+++ src/components/ViewHome.vue
@@ -1,18 +1,37 @@
 <template>
   <v-app>
     <v-main>
-      <h1>Soy Home</h1>
+      <v-carousel>
+        <v-carousel-item src="../assets/img/Home7.jpg" cover></v-carousel-item>
+        <v-carousel-item src="../assets/img/Home3.jpg" cover></v-carousel-item>
+        <v-carousel-item src="../assets/img/Home5.jpg" cover></v-carousel-item>
+        <v-carousel-item src="../assets/img/home12.jpg" cover></v-carousel-item>
+      </v-carousel>
+
+      <!-- <v-carousel cycle height="400" hide-delimiter-background show-arrows="hover">
+        <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slides.src">
+          <v-sheet :color="colors[i]" height="100%">
+            <div class="d-flex fill-height justify-center align-center">
+              <v-img cover>{{ slide }}</v-img>
+            </div>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel> -->
     </v-main>
+
+
+    
+
   </v-app>
 
-  <v-footer app dark height="auto" class="footer justify-center" >
+  <v-footer app dark height="auto" class="footer justify-center">
     <v-container fluid>
       <v-row text-align="center">
         <v-col>
           <v-card class="flex" flat tile>
 
             <v-card-actions class="grey darken-3 justify-center">
-              &copy;2023 — <strong>CGS2</strong>
+              &copy;2023 — <strong>Grupo CGS2 SaTovar/CJimenez/JSBuitrago</strong>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -24,6 +43,25 @@
 <script>
 export default {
   data: () => ({
+    colors: [
+      'indigo',
+      'warning',
+      'pink darken-2',
+    ],
+    slides: [
+
+      {
+        src: '../assets/img/Home3.jpg',
+      },
+      {
+        src: "../assets/img/Home2.jpg",
+      },
+      {
+        src: "../assets/img/Home1.jpg",
+      },
+
+    ],
+
     icons: [
       'fab fa-facebook',
       'fab fa-twitter',
@@ -34,6 +72,7 @@ export default {
   })
 }
 </script>
+
 
 <style>
 .footer {
